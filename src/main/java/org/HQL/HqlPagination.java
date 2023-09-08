1d262b47987dc15dac8bc7019335afd5db43f947
Shivam <shivamtech899@gmail.com>
2023-09-08 18:30:04 +0530

    HQL Pagination is done.

---
+++ src/main/java/org/HQL/HqlPagination.java
@@ -0,0 +1,39 @@
+package org.HQL;
+
+import org.example.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class HqlPagination
+{
+    public static void main(String[] args)
+    {
+        SessionFactory factory= new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Query query = session.createQuery("from Student");
+
+        // implementing pagination using Hibernate
+
+        query.setFirstResult(4); // this defines from which row you are starting.
+         query.setMaxResults(2); // this defines how many rows per page you needed.
+
+        List<Student> resultList = query.getResultList();
+
+        for (Student st: resultList)
+        {
+            System.out.println(st.getId()+"  "+st.getName()+"   "+st.getCity());
+            
+        }
+
+
+
+        session.close();
+        factory.close();
+
+    }
+}
